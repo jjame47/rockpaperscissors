8904773d3c5bbca6378832d9a059071694f35048
Jordan <jjame47@proton.me>
2022-08-26 08:14:40 -0500

    Updated game to use UI instead of console

--- rps.js
+++ rps.js
@@ -3,48 +3,34 @@ let playerWins = 0;
 let playerLosses = 0;
 let playerTies = 0;
 
+loadElements();
+
 function getComputerChoice(){
     let choiceInt = Math.floor(Math.random() * validOptions.length);
-    return choiceInt;
-}
-
-function getPlayerChoice(){
-    while(true){
-        let promptString = "Choose one of the following:\n\n";
-        for(let x = 0; x < validOptions.length; x++){
-            promptString += `${validOptions[x]}\n`;
-        }
-        let choice = prompt(promptString).toLowerCase().trim();
-        let choiceInt = validOptions.indexOf(choice);
-        if (choiceInt >= 0){
-            return choiceInt;
-        }
-        alert(`${choice} was not valid. Please try again.`);
-    }
+    return validOptions[choiceInt];
 }
 
 function getResults(playerChoice, computerChoice){
-    const indexMax = validOptions.length - 1;
     let result = "";
-
-    console.log(`You selected: ${validOptions[playerChoice]}. Computer selected: ${validOptions[computerChoice]}`);
 
     if(playerChoice == computerChoice){
         result = "T";
     }
-    else if(playerChoice == 0 && computerChoice == indexMax){
+    else if(playerChoice == 'rock' && computerChoice == 'scissors'){
         result = "W";
     }
-    else if(computerChoice == 0 && playerChoice == indexMax){
-        result = "L";
+    else if(playerChoice == 'paper' && computerChoice == 'rock'){
+        result = "W";
     }
-    else if(playerChoice < computerChoice){
-        result = "L";
+    else if(playerChoice == 'scissors' && computerChoice == 'paper'){
+        result = "W";
     }
     else {
-        result = "W";
+        result = "L";
     }
-    console.log(result);
+    const pRound = document.querySelector('.round');
+    pRound.textContent = `Result: ${result}. You selected: ${playerChoice}. Computer selected: ${computerChoice}`;
+
     return result;
 }
 
@@ -52,30 +38,70 @@ function updateRecord(result){
     if(result == "W"){playerWins++};
     if(result == "L"){playerLosses++};
     if(result =="T"){playerTies++};
+    
+    const pRecord = document.querySelector('.record');
+    pRecord.textContent = `Current record is ${playerWins} win(s), ${playerLosses} loss(es), ${playerTies} tie(s)`;
+
 }
 
-function playRound(){
+function playRound(playerChoice){
     computerChoice = getComputerChoice();
-    playerChoice = getPlayerChoice();
     result = getResults(playerChoice, computerChoice);
     updateRecord(result);
 }
 
-function printRecord(){
-    console.log(`Current record is ${playerWins} win(s), ${playerLosses} loss(es), ${playerTies} tie(s)`);
+function loadElements(){
+    const body = document.querySelector('body');
+
+    const h1 = document.createElement('h1');
+    h1.textContent = 'Rock Paper Scissors';
+    body.appendChild(h1);
+    
+    const divGame = document.createElement('div');
+    
+    const buttonRock = document.createElement('button');
+    buttonRock.textContent = 'Rock';
+    buttonRock.id = 'rock';
+    divGame.appendChild(buttonRock);
+    
+    const buttonPaper = document.createElement('button');
+    buttonPaper.textContent = 'Paper';
+    buttonPaper.id = 'paper';
+    divGame.appendChild(buttonPaper);
+    
+    const buttonScissors = document.createElement('button');
+    buttonScissors.textContent = 'Scissors';
+    buttonScissors.id = 'scissors';
+    divGame.appendChild(buttonScissors);
+    
+    body.appendChild(divGame);
+    
+    const gameButtons = divGame.querySelectorAll('button');
+    gameButtons.forEach((button) => {
+        button.addEventListener('click', () => {
+            playRound(button.id);
+        });
+    });
+
+    const divResults = document.querySelector('div');
+
+    const pRound = document.createElement('p');
+    pRound.classList.add('round');
+    pRound.style.color = 'Red';
+    pRound.style.fontSize = '32px';
+    pRound.textContent = 'Click any button to play!';
+    divResults.appendChild(pRound);
+
+    const pRecord = document.createElement('p');
+    pRecord.classList.add('record');
+    pRecord.style.color = 'Black';
+    pRecord.style.fontSize = '32px';
+    pRecord.textContent = '0 win(s), 0 loss(es), 0 tie(s)';
+    divResults.appendChild(pRecord);
+
+    body.appendChild(divResults);
+    
 }
 
-function game(){
-    console.log('Welcome to Rock Paper Scissors');
-    while(true){
-        playRound();
-        printRecord();
-        playAgain = prompt('Play again (y/n)?').toLowerCase().trim();
-        if(playAgain != 'y'){
-            console.log('Thank you for playing, goodbye!');
-            break;
-        }
-    }
-}
 
-game();
+
